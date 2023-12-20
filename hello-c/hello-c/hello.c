948abf9958ea24e9132eb17c6e62bbc1e3e4265d
eunchae <ddolchae513@gmail.com>
2023-12-20 14:36:47 +0900

    Hello C

---
+++ hello-c/hello-c/hello.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  hello-c
+//
+//  Created by 정은채 on 12/20/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
